d72cec9dad9ccd8b5a012adf708abf9f59224f49
Afton Downey <aaike001@gmail.com>
2016-01-20 09:54:40 -0800

    pass first test to identify vowels

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// var string = "afton"
+ var ay = "ay"
+
+var pigLatin = function(string) {
+  // string = string.toLowerCase();
+  var first = string.charAt(0);
+
+  if (first === "a" || first === "e" || first === "i" || first === "o" || first === "u" ) {
+    return true;
+  };
+
+};
---
+++ spec/specs.js
@@ -0,0 +1,9 @@
+
+describe('pigLatin', function() {
+
+  it("checking if a string starts with a vowel", function() {
+    expect(pigLatin("afton")).to.equal(true);
+  });
+
+
+});
